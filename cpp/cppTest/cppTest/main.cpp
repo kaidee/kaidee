8b0146bdf5fdc377741600b3df6d014b15ff3082
kaidee <lkaidee@gmail.com>
2014-05-30 21:09:25 +0800

    Initial Commit

---
+++ cpp/cppTest/cppTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cppTest
+//
+//  Created by Kaidee Lee on 5/30/14.
+//  Copyright (c) 2014 Kaidee Lee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
